9697a4e8c31bc5af73f1af5cc688bfc485a138bc
github username <crishanks12@gmail.com>
2019-03-13 20:18:52 -0700

    fetched dog data, rendered dog images on page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', main);
+const DOG_SERVER = "https://dog.ceo/api/breeds/image/random/4";
+let DOG_RESULTS = [];
+
+function main() {
+  fetch(DOG_SERVER)
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
+    DOG_RESULTS = json.message;
+    renderDogImages(DOG_RESULTS);
+  });
+}
+
+function renderDogImages(dogImageUrls) {
+  const dogDiv = document.getElementById("dog-image-container");
+  for (let url of dogImageUrls) {
+    let img = document.createElement('img');
+    img.src = url;
+    dogDiv.appendChild(img);
+  }
+}
+
